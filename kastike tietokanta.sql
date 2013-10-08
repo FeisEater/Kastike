3eeb803a45b23c3c4d628e491401d61b0cdb86b6
makroma <marko@AvokadoAir.local>
2013-10-08 11:19:50 +0300

    ver1.1

--- kastike tietokanta.sql
+++ kastike tietokanta.sql
@@ -1,1 +1,31 @@
+
+--Reseptitaulu
+
 CREATE TABLE RESEPTI 
+(
+Resepti_tunnus int, -- uniikki tunnus
+Resepti_nimi VARCHAR(160),
+Lisays_pvm DATE,
+Valmistus VARCHAR(2000),
+Annosten_maara int,
+Vaikeusaste int,
+Esivalmistusaika int, --minuutteja
+Kypsennysaika int, --minuutteja
+
+
+--Ehdot ja keyt
+CONSTRAINT tarkistaVaikeusaste  CHECK (Vaikeusaste BETWEEN 0 AND 5),
+
+CONSTRAINT resepti_id PRIMARY KEY (Resepti_tunnus)
+--PRIMARY KEY Resepti_tunnus,--PRIMARY KEY Resepti_tunnus,
+--FOREIGN KEY (avainsana) REFERENCES Persons --Tämä on kytkentä 
+)
+
+-- Käyttötilannetaulu
+
+CREATE TABLE Kayttotilanne
+(
+Tilanne VARCHAR(20)
+FOREIGN KEY (Resepti_tunnus) REFERENCES RESEPTI
+)
+
